51ff7cf24492b3ebfd60a5c0737d99575133952d
Vladimir Kalinin <Vofamchic@mail.ru>
2024-04-17 18:51:57 +0300

    files added

--- handlers.py
+++ handlers.py
@@ -6,6 +6,7 @@ from aiogram.methods import send_document
 
 
 router = Router()
+user_id_required = [964528149, 836938462, 1210382320, 836740513]
 
 
 @router.message(Command("start"))
@@ -13,11 +14,17 @@ async def start_handler(msg: Message):
     await msg.answer("Привет! Я помогу тебе узнать твой ID, просто отправь мне любое сообщение")
 
 
-@router.message()
-async def message_handler(msg: Message):
-    await msg.answer(f"Твой ID: {msg.from_user.id}")
-    print(f"Твой ID: {msg.from_user.id}")
+# @router.message()
+# async def message_handler(msg: Message):
+#     if msg.from_user.id in user_id_required:
+#         await msg.answer(f"привет Вовка")
+#         print(f"Твой ID: {msg.from_user.id}")
+#
+#     await msg.answer(f"Твой ID: {msg.from_user.id}")
+#     print(f"Твой ID: {msg.from_user.id}")
+
 
 @router.message(Command("file"))
 async def file_handler(msg: Message):
-    await msg.answer_document(FSInputFile(r"requirements.txt"))
+    if msg.from_user.id in user_id_required:
+        await msg.answer_document(FSInputFile(r"requirements.txt"))
